5f5be78546b157d9c93474b3aa359e32def6774f
Leandro Alves <lealvesac@gmail.com>
2022-02-11 13:01:27 -0300

    print formatado printf

--- aula06a.py
+++ aula06a.py
@@ -3,4 +3,4 @@
 n1 = int(input('Digite um valor: '))
 n2 = int(input('Digite outro valor'))
 s = n1 + n2
-print('A Soma entre {} e {} vale {}'.format(n1, n2, s))
+print(f'A Soma entre {n1} e {n2} vale {s}')
